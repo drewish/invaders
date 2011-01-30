0e811f4d5cc4a99d6783c442728cf1e4663f5ea1
andrew morton <graveltech@gmail.com>
2011-01-30 12:01:47 -0500

    Moved the now, unused hex line drawing code out of the way.

--- board_view.rb
+++ board_view.rb
@@ -13,37 +13,8 @@ class BoardView < BaseView
     @board = board
     @board.add_observer(self)
 
-    @board.tiles.each_value { | o | @render_list << TileView.new(o) }
-    @board.intersections.each_value { | o | @render_list << IntersectionView.new(o) }
-    @board.paths.each_value { | o | @render_list << PathView.new(o) }
-  end
-
-  def render_hex_lines(s)
-    glBegin(GL_LINES)
-
-    w = s * 10
-    h = s * 10
-
-    # Vertical lines
-    xoffset = Math.cos(Math::PI / 6) * s
-    (-10..10).each do |i|
-      x = i * xoffset
-      glVertex2f(x, -h); glVertex2f(x, h)
-    end
-
-    # Diagonal up
-    yoffset = Math.tan(Math::PI / 6) * w
-    (-30..10).each do |i|
-      y = i * s;
-      glVertex2f(-w, y - yoffset); glVertex2f(w, y + yoffset)
-    end
-
-    # Diagonal down
-    (-10..30).each do |i|
-      y = i * s;
-      glVertex2f(-w, y + yoffset); glVertex2f(w, y - yoffset)
-    end
-
-    glEnd()
+    @render_list = @board.tiles.collect { | k, o | TileView.new(o) }
+    @render_list += @board.intersections.collect { | k, o | IntersectionView.new(o) }
+    @render_list += @board.paths.collect { | k, o | PathView.new(o) }
   end
 end
--- hexcube.rb
+++ hexcube.rb
@@ -1,4 +1,33 @@
+def render_hex_lines(s)
+  glBegin(GL_LINES)
 
+  w = s * 10
+  h = s * 10
+
+  # Vertical lines
+  xoffset = Math.cos(Math::PI / 6) * s
+  (-10..10).each do |i|
+    x = i * xoffset
+    glVertex2f(x, -h); glVertex2f(x, h)
+  end
+
+  # Diagonal up
+  yoffset = Math.tan(Math::PI / 6) * w
+  (-30..10).each do |i|
+    y = i * s;
+    glVertex2f(-w, y - yoffset); glVertex2f(w, y + yoffset)
+  end
+
+  # Diagonal down
+  (-10..30).each do |i|
+    y = i * s;
+    glVertex2f(-w, y + yoffset); glVertex2f(w, y - yoffset)
+  end
+
+  glEnd()
+end
+  
+def render_hex_tile
   x60 = Math.sqrt(3)/2
   hex = [[0, 1],
 	[-x60, 0.5],
@@ -32,4 +61,4 @@
 puts indices.inspect
     glDrawElements(GL_QUAD_STRIP, 4, GL_UNSIGNED_INT, indices.pack("I*"))
   end
-
+end
