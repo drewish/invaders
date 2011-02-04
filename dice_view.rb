bc7513f25ab463f940c93d7e55aed1ad27ed4dde
andrew morton <graveltech@gmail.com>
2011-02-04 09:57:48 -0500

    Playing around with making the dice 3D.

--- dice_view.rb
+++ dice_view.rb
@@ -11,18 +11,62 @@ class DiceView < BaseView
     @dice = model
   end
 
+  def drawDot(x, y)
+    qobj = gluNewQuadric()
+    gluQuadricDrawStyle(qobj, GLU_FILL)
+    glPushMatrix()
+    glRotate(30, 0, 0, 1)
+    gluDisk(qobj, 0, 0.1, 10, 1)
+    glPopMatrix()
+  end
+
+  def draw1
+    glPushMatrix()
+    glRotate(30, 0, 0, 1)
+    drawDot 0, 0
+    glPopMatrix()
+  end
+  
+  def draw3
+    gl
+  end
+
+
   def render
     @dice.values.each_index do | i |
       glPushMatrix()
       
       glTranslate(-2 + 2 * i, -9, 0)
 
+#compute the seven points on the dice in the shape of an H
+      lt = [];
+
       glColor(1, 1, 1)
+=begin
       glBegin(GL_POLYGON)
       glVertex2f(1, 1); glVertex2f(1, -1)
       glVertex2f(-1, -1); glVertex2f(-1, 1)
       glEnd()
+=end
+      glutSolidCube(1)
 
+      glColor(0,0,0)
+      drawDot(0,0)
+=begin
+      d1()
+      rY
+      d3
+      rY
+      d6
+      rY
+      d4
+      rX
+      d2
+      r?
+      r?
+      d5
+=end
+      
       # Text
       glColor(0, 0, 0)
       s = @dice.values[i].to_s
